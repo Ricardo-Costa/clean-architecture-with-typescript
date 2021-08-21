963653bbd75dbb48956a717f853a7749846f0696
Ricardo Costa <ricardo18acs@gmail.com>
2021-08-20 22:06:32 -0300

    Improve router interface

--- src/presentation/interfaces/router-interface.ts
+++ src/presentation/interfaces/router-interface.ts
@@ -1,8 +1,12 @@
 import AuthUseCase from "../../domain/usecases/auth-usecase";
 import HttpRequest from "../utils/http-request-util";
 import HttpResponse from "../utils/http-response-util";
+import { EmailValidator } from "../validators/email.validator";
 
 export default interface Router {
-    readonly authUseCase: AuthUseCase
+
+    readonly authUseCase: AuthUseCase,
+    readonly emailValidator: EmailValidator
+
     router: (httpRequest: HttpRequest) => HttpResponse
 }
