306625c2efcde1fe4593c95ee913cb74cab50d1a
Ricardo Costa <ricardo18acs@gmail.com>
2021-08-21 20:18:08 -0300

    Test create users

--- src/domain/repositories/user-repository.spec.ts
+++ src/domain/repositories/user-repository.spec.ts
@@ -1,12 +1,13 @@
-import {
-    EMAIL,
-    PASSWORD,
-    mockUsers
- } from "../../../__mocks__/user.sut"
+import { NAME, EMAIL, PASSWORD } from "../../../__mocks__/user.sut"
 import User from "../entities/user-entity"
 import UserRepository from "./user-repository"
 
 describe('User Repository', () => {
+
+    test('Should to create an User.', () => {
+        const user:  User = UserRepository.create(NAME, EMAIL, PASSWORD)
+        expect(user).toBeInstanceOf(User)
+    })
 
     test('Should to find an User by email and password.', () => {
         const user:  User | null = UserRepository.findOneByEmailAndPassword(EMAIL, PASSWORD)
